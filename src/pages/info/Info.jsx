e2df8e85d43266d64b3368e03282c70d8e3e4e60
Guilherme Gomes <gguife747@gmail.com>
2023-06-14 17:12:43 -0300

    deixando Info totalmente funcional

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -1,9 +1,81 @@
 import './Info.css'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Info = () => {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [nextPage, setNextPage] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    setNextPage(name !== '' && email !== '' && phoneNumber !== '')
+  }, [name, email, phoneNumber])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    
+    if(!nextPage){
+      if(name === '' ){
+        setErrorMessage('This field is required')
+      }else if(email === '' ){
+        setErrorMessage('This field is required')
+      }else if(phoneNumber === '' ){
+        setErrorMessage('This field is required')
+      }
+    }
+    
+    if(nextPage){
+      navigate('/plan')
+    }
+  }
+  
+
   return (
-    <div className='Info'>
-      Info
+    <div className='info'>
+        <h2>Personal info</h2>
+        <p>Please provide your name, email address and phone number.</p>
+        <form action="" onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          {name === '' && <span className='errorMessage-name'>{errorMessage}</span>}
+          <input
+            type="text" 
+            id='name' 
+            name='name'
+            value={name} 
+            placeholder='Gguife' 
+            onChange={(e) => setName(e.target.value)}
+            style={{border: errorMessage && name === '' ? '1px solid red' : '1px solid hsl(229, 24%, 87%)'}}
+          />
+          
+          <label htmlFor="email">Email Address</label>
+          {email === '' && <span className='errorMessage-email'>{errorMessage}</span>}
+          <input 
+            type="email" 
+            name='email' 
+            id='email'
+            value={email} 
+            placeholder='gguife747@gmail.com' 
+            onChange={(e) => setEmail(e.target.value)} 
+            style={{border: errorMessage && email === '' ? '1px solid red' : '1px solid hsl(229, 24%, 87%)'}}
+          />
+          <label htmlFor="phone">Phone Number</label>
+          {phoneNumber === '' && <span className='errorMessage-phone'>{errorMessage}</span>}
+          <input 
+            type="number" 
+            name='phone' 
+            id='phone' 
+            value={phoneNumber}
+            placeholder='+55 (99) 99999-9999' 
+            onChange={(e) => setPhoneNumber(e.target.value)} 
+            style={{border: errorMessage && phoneNumber === '' ? '1px solid red' : '1px solid hsl(229, 24%, 87%)'}}
+          />
+
+            <button type='submit'>Next Step</button>
+        </form>
     </div>
   )
 }
