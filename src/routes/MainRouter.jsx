5c84ea1f976c4a541c0338cbaea65423782b6519
Guilherme Gomes <gguife747@gmail.com>
2023-06-22 15:28:23 -0300

    Terminando a criacao de rotas

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,16 +1,21 @@
-import { useRoutes } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom';
 
 //Pages
-import Info from '../pages/info/Info'
-import Plan from '../pages/plan/Plan'
+import Info from '../pages/info/Info';
+import Plan from '../pages/plan/Plan';
+import AddOns from '../pages/addOns/AddOns';
+import Summary from '../pages/summary/Summary';
 
 const MainRouter = () => {
   const router = useRoutes([
     {path:'/', element: <Info />},
     {path:'/plan', element: <Plan />},
+    {path:'/addOns', element: <AddOns />},
+    {path:'/addOns', element: <AddOns />},
+    {path:'/summary', element: <Summary />},
   ])
 
-  return router
+  return router;
 }
 
-export default MainRouter
+export default MainRouter;
