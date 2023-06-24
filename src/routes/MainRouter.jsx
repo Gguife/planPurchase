e37b848a993aff37f10de6061660209b43ea1101
Guilherme Gomes <gguife747@gmail.com>
2023-06-24 18:32:46 -0300

    atualizando as rotas

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -3,16 +3,20 @@ import { useRoutes } from 'react-router-dom';
 //Pages
 import Info from '../pages/info/Info';
 import Plan from '../pages/plan/Plan';
+import PlanYear from '../pages/plan/plaYear/PlanYear';
 import AddOns from '../pages/addOns/AddOns';
 import Summary from '../pages/summary/Summary';
+import SummaryYear from '../pages/summary/summaryYear/SummaryYear';
+
 
 const MainRouter = () => {
   const router = useRoutes([
     {path:'/', element: <Info />},
     {path:'/plan', element: <Plan />},
-    {path:'/addOns', element: <AddOns />},
+    {path:'/planyear', element: <PlanYear />},
     {path:'/addOns', element: <AddOns />},
     {path:'/summary', element: <Summary />},
+    {path:'/summaryear', element: <SummaryYear />},
   ])
 
   return router;
