c652b50e537aba47ee435d37622befc1cbea4ebc
Guilherme Gomes <gguife747@gmail.com>
2023-06-28 16:44:48 -0300

    Adicionei mais rotas

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -7,14 +7,15 @@ import PlanYear from '../pages/plan/plaYear/PlanYear';
 import AddOns from '../pages/addOns/AddOns';
 import Summary from '../pages/summary/Summary';
 import SummaryYear from '../pages/summary/summaryYear/SummaryYear';
-
+import AddOnsYear from '../pages/addOns/addOnsYear/AddOnsYear'
 
 const MainRouter = () => {
   const router = useRoutes([
     {path:'/', element: <Info />},
     {path:'/plan', element: <Plan />},
     {path:'/planyear', element: <PlanYear />},
-    {path:'/addOns', element: <AddOns />},
+    {path:'/addons', element: <AddOns />},
+    {path:'/addonsyear', element: <AddOnsYear />},
     {path:'/summary', element: <Summary />},
     {path:'/summaryear', element: <SummaryYear />},
   ])
