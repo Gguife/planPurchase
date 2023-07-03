a34a45174a31a14e6037fffd681d83a39aa3e03e
Guilherme Gomes <gguife747@gmail.com>
2023-07-03 12:13:54 -0300

    Pagina Summary finalizada

--- src/pages/summary/Summary.jsx
+++ src/pages/summary/Summary.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import './Summary.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Summary = () => {
+  const navigate = useNavigate();
+  
+  const navigateToConfirm = () =>{
+    navigate('/complete');
+  }
+
   return (
-    <div>Summary</div>
+    <div className='summary'>
+      <h2>Finishing up</h2>
+      <p className='summary-subtitle'>Double-check everything looks OK before confirming.</p>
+      <div className="summary-container">
+        <div className="summary-plan">
+          <h3>Arcade (Monthly)</h3>
+          <span>$9/mo</span>
+        </div>
+        <div className="hr">
+          <hr />
+        </div>
+        <div className="summary-addons">
+          <div className="addons-content">
+            <p>Online service</p>
+            <span>+$1/mo</span>
+          </div>
+          <div className="addons-content">
+            <p>Larger storage</p>
+            <span>+$2/mo</span>
+          </div>
+        </div>
+        <div className="summary-total">
+          <p>Total (per month)</p>
+          <span className='total'>+12/mo</span>
+        </div>
+      </div>
+
+      <div className="summary-routes">
+        <Link to='/addons'>Go back</Link>
+        <button onClick={navigateToConfirm}>Confirm</button>
+      </div>
+    </div>
   )
 }
 
-export default Summary
+export default Summary;
--- src/pages/summary/summaryYear/SummaryYear.jsx
+++ src/pages/summary/summaryYear/SummaryYear.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const SummaryYear = () => {
+  const navigate = useNavigate();
+  
+  const navigateToConfirm = () =>{
+    navigate('/complete');
+  }
   return (
-    <div>SummaryYear</div>
+    <div className='summary'>
+    <h2>Finishing up</h2>
+    <p className='summary-subtitle'>Double-check everything looks OK before confirming.</p>
+    <div className="summary-container">
+      <div className="summary-plan">
+        <h3>Arcade (Yearly)</h3>
+        <span>$90/mo</span>
+      </div>
+      <div className="hr">
+        <hr />
+      </div>
+      <div className="summary-addons">
+        <div className="addons-content">
+          <p>Online service</p>
+          <span>+$10/mo</span>
+        </div>
+        <div className="addons-content">
+          <p>Larger storage</p>
+          <span>+$20/mo</span>
+        </div>
+      </div>
+      <div className="summary-total">
+        <p>Total (per month)</p>
+        <span className='total'>+120/mo</span>
+      </div>
+    </div>
+
+    <div className="summary-routes">
+      <Link to='/addons'>Go back</Link>
+      <button onClick={navigateToConfirm}>Confirm</button>
+    </div>
+  </div>
   )
 }
 
